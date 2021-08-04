6c3bb58737a6965ae2327bd04e2eae2ce3ecdbde
Agatha Wong <41976437+eggiepiggie@users.noreply.github.com>
2021-08-04 13:19:08 -0400

    Add shopify.config.js

---
+++ shopify.config.js
@@ -0,0 +1,7 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'eggie-craft-avenue.myshopify.com',
+  storefrontToken: '829689dffbabe6acf7ca0053b48678cc',
+  graphqlApiVersion: 'unstable',
+};
+
