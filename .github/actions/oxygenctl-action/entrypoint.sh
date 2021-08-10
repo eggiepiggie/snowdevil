df691e6520eca8282dc1b4f8961a236d378d5bd3
Agatha Wong <agatha.wong@shopify.com>
2021-08-10 15:41:27 -0400

    Put into entrypoint script

--- .github/actions/oxygenctl-action/entrypoint.sh
+++ .github/actions/oxygenctl-action/entrypoint.sh
@@ -3,13 +3,14 @@
 export OXYGEN_DEPLOYMENT_TOKEN="$INPUT_OXYGEN_DEPLOYMENT_TOKEN"
 export COMMIT_SHA="$INPUT_SHA"
 
+# Read hostname from Hydrogen configuration file
+export STORE_HOSTNAME=$(sed -n "s/.*storeDomain.*'\(.*\)'.*/\1/p" shopify.config.js)
+
 oxygenctl --version
 oxygenctl deploy \
   --assets-dir "$INPUT_OXYGEN_ASSETS_DIR" \
   --worker-file "$INPUT_OXYGEN_WORKER_FILE" \
   --dms-address "$INPUT_OXYGEN_DMS_ADDRESS"
-
-echo $STORE_HOSTNAME
 
 # Hardcoded URL for now
 echo "::set-output name=url::https://"${COMMIT_SHA:0:12}"-"$STORE_HOSTNAME".myshopify.dev"
