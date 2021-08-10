19b266fc2aaba0570a7deefb53d0f10df8111af5
Agatha Wong <agatha.wong@shopify.com>
2021-08-10 19:53:41 -0400

    put store domain back in

--- shopify.config.js
+++ shopify.config.js
@@ -1,6 +1,6 @@
 export default {
   locale: 'en-us',
-  storeDomain: '',
+  storeDomain: 'joshs-build-a-store.myshopify.com',
   storefrontToken: '60d0972e467c5797d5b988f66e82c4e3',
   graphqlApiVersion: 'unstable',
 };
