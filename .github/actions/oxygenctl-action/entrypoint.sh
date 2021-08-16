cd3efdbcf161fcd3ae7fb92af50e92500c466ae3
Agatha Wong <agatha.wong@shopify.com>
2021-08-16 11:18:56 -0400

    Test new oxygenctl

--- .github/actions/oxygenctl-action/entrypoint.sh
+++ .github/actions/oxygenctl-action/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 export OXYGEN_DEPLOYMENT_TOKEN="$INPUT_OXYGEN_DEPLOYMENT_TOKEN"
-export COMMIT_SHA="$INPUT_SHA"
 
 # Read hostname from Hydrogen configuration file
 export OXYGEN_STORE_DOMAIN=$(sed -n "s/.*storeDomain.*'\(.*\)'.*/\1/p" shopify.config.js)
@@ -15,7 +14,8 @@ oxygenctl --version
 oxygenctl deploy \
   --assets-dir "$INPUT_OXYGEN_ASSETS_DIR" \
   --worker-file "$INPUT_OXYGEN_WORKER_FILE" \
-  --dms-address "$INPUT_OXYGEN_DMS_ADDRESS"
+  --dms-address "$INPUT_OXYGEN_DMS_ADDRESS" \
+  --store-domain "test-preview.myshopify.com"
 
 # Hardcoded URL for now
-echo "::set-output name=url::https://"${GITHUB_SHA:0:12}"-oxygen-sws.myshopify.dev"
+echo "::set-output name=url::https://"${GITHUB_SHA:0:12}"-test-preview.myshopify.dev"
